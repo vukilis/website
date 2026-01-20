5ddc4e8c2007b83d5e42960b6c750edbba32fd0b
vukilis <vuk.lekic@vukilis.com>
2026-01-20 02:39:01 +0100

    cloudflare

--- build.sh
+++ build.sh
@@ -1,30 +1,32 @@
 #!/bin/bash
 
-# Define version from your screenshot
-HUGO_VERSION=0.115.0
-export TZ=Europe/Belgrade
+main() {
+    # Define version from your screenshot
+    HUGO_VERSION=0.115.0
+    export TZ=Europe/Belgrade
 
-# Install Hugo Extended
-echo "Installing Hugo v${HUGO_VERSION}..."
-curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
-tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
-cp hugo /opt/buildhome
-rm LICENCE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+    # Install Hugo Extended
+    echo "Installing Hugo v${HUGO_VERSION}..."
+    curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+    tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
+    cp hugo /opt/buildhome
+    rm LICENCE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
 
-# Verify installed version
-echo "Verifying installation..."
-echo Go: "$(go version)"
-echo Hugo: "$(./hugo version)"
-echo Node: "$(node --version)"
+    # Verify installed version
+    echo "Verifying installation..."
+    echo Go: "$(go version)"
+    echo Hugo: "$(./hugo version)"
+    echo Node: "$(node --version)"
 
-# Clone themes/submodules (Blowfish theme)
-echo "Updating submodules..."
-git submodule update --init --recursive
-git config core.quotepath false
+    # Clone themes/submodules (Blowfish theme)
+    echo "Updating submodules..."
+    git submodule update --init --recursive
+    git config core.quotepath false
 
-# Build the website
-echo "Building the Site..."
-./hugo --gc --minify
+    # Build the website
+    echo "Building the Site..."
+    ./hugo --gc --minify
+}
 
 set -euo pipefail
 main "$@"
