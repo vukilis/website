f3639d0118477bee09d33a2aa2357b7b5633d21a
vukilis <vuk.lekic@vukilis.com>
2026-01-20 02:44:03 +0100

    cloudflare

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ main() {
     curl -LJO https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
     tar -xf "hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz"
     cp hugo /opt/buildhome
-    rm LICENCE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
+    rm LICENSE README.md hugo_extended_${HUGO_VERSION}_linux-amd64.tar.gz
 
     # Verify installed version
     echo "Verifying installation..."
