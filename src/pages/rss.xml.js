8bbe33c3bbd8d78f37719da40ec2967f4ad06fb8
vukilis <vuk.lekic@vukilis.com>
2026-08-06 22:42:19 +0200

    fix(rss): sort blog posts by publication date in RSS feed

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -2,7 +2,7 @@ import rss from '@astrojs/rss';
 import { getCollection } from 'astro:content';
 
 export async function GET(context) {
-  const posts = await getCollection('blog');
+  const posts = (await getCollection('blog')).sort((a, b) => b.data.pubDate - a.data.pubDate);
   return rss({
     title: 'vukilis.com',
     description: 'Blog feed',
